71382f332fb87505a9815dfb62175e938eedfddf
sarthakgarg77 <80844795+sarthakgarg77@users.noreply.github.com>
2022-04-09 00:37:55 +0530

    Update rps_app.py

--- rps_app.py
+++ rps_app.py
@@ -17,7 +17,7 @@ def import_and_predict(image_data, model):
         
         return prediction
 
-model = tf.keras.models.load_model('my_model.hdf5')
+model = tf.keras.models.load_weights('my_model.hdf5')
 
 st.write("""
          # Rock-Paper-Scissor Hand Sign Prediction
