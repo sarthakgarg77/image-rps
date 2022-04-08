20a1720287ef5db437b8b9b12c274e3b62395cc4
sarthakgarg77 <80844795+sarthakgarg77@users.noreply.github.com>
2022-04-09 00:10:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.streamlit/                                               
 echo "\                       
-[server]\n\                       
+[server]\n\   
+headless = true\n\   
 port = $PORT\n\                       
 enableCORS = false\n\                       
-headless = true\n\                       
 \n\                       
 " > ~/.streamlit/config.toml
